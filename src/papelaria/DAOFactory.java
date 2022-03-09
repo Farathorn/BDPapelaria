68fefe87108ce772c3b2c888178fd509b9ea92a7
Farathorn <lucaskavalkantej@gmail.com>
2022-03-09 17:47:11 -0400

    DAO Fix

--- src/papelaria/DAOFactory.java
+++ src/papelaria/DAOFactory.java
@@ -1,12 +1,12 @@
 package papelaria;
 
 import papelaria.dao.TabelaDAO;
-import papelaria.dao.mySQL.TabelaDAOImpl;
+import papelaria.dao.mySQL.TabelaDAOmySQL;
 
 public class DAOFactory {
 
 	public static TabelaDAO createDAO() {
 	
-		return new TabelaDAOImpl();
+		return new TabelaDAOmySQL();
 	}
 }
--- src/papelaria/dao/TabelaDAO.java
+++ src/papelaria/dao/TabelaDAO.java
@@ -9,28 +9,7 @@ import java.util.List;
 import papelaria.entidades.Entidade;
 import papelaria.entidades.EntidadeForte;
 
-public abstract class TabelaDAO {
-
-	protected Connection connection;
-	
-	protected boolean abreConexao () {
-		
-		try {
-			
-			connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/lucasbd", "root", "123");
-			return true;
-		}
-		catch (SQLTimeoutException sqlTimeoutException) {
-			
-			System.out.print("CONNECTION TIMEOUT\n");
-		}
-		catch (SQLException sqlException) {
-			
-			System.out.print("DATABASE ACCESS ERROR\n");
-		}
-		
-		return false;
-	}
+public interface TabelaDAO {
 	
 	public abstract boolean adicionar (Entidade t, String where, String[] fields, String[] values);
 	
