0ef6dce35641b0416619ecb33027b6c4d22b83ec
Farathorn <lucaskavalkantej@gmail.com>
2022-03-09 17:35:45 -0400

    mySQL

---
+++ mySQL_BD/Criação.sql
@@ -0,0 +1,112 @@
+CREATE DATABASE Papel_E_Art;
+
+CREATE TABLE Franquia (
+	Código INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Endereço VARCHAR(500) NOT NULL,
+    Cofre FLOAT8 NOT NULL
+);
+
+CREATE TABLE Shopping (
+	Código INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(100) NOT NULL,
+    Endereço VARCHAR(500) NOT NULL UNIQUE KEY,
+    Aluguel FLOAT8 NOT NULL
+);
+
+CREATE TABLE Estoque (
+	Código INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Contato VARCHAR(120) NOT NULL,
+    Endereço VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE Fornecedor (
+	Código INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Contato VARCHAR(120) NOT NULL,
+    Endereço VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE Funcionário (
+	CPF VARCHAR(11) NOT NULL PRIMARY KEY,
+    Nome VARCHAR(120) NOT NULL,
+    Carteira_de_Trabalho VARCHAR(12) NOT NULL UNIQUE KEY,
+    Cargo VARCHAR(25) NOT NULL,
+    Expediente VARCHAR(25) NOT NULL,
+    Salário_Base FLOAT8 NOT NULL,
+    Faltas INTEGER NOT NULL,
+    Venda_do_Mês FLOAT8 NOT NULL,
+    Ticket_Médio FLOAT8 NOT NULL,
+    Franquia INTEGER NOT NULL,
+    CONSTRAINT FOREIGN KEY (Franquia) REFERENCES Franquia(Código)
+);
+
+CREATE TABLE Custo_Direto (
+	Valor FLOAT8 NOT NULL,
+    Descrição VARCHAR(200),
+    Shopping INTEGER NOT NULL,
+    CONSTRAINT FOREIGN KEY (Shopping) REFERENCES Shopping(Código)
+);
+
+CREATE TABLE Custo_Indireto (
+	Valor FLOAT8 NOT NULL,
+    Descrição VARCHAR(200),
+    Fornecedor INTEGER,
+    Estoque INTEGER,
+    CONSTRAINT Fornecedor FOREIGN KEY (Fornecedor) REFERENCES Fornecedor(Código),
+    CONSTRAINT Estoque FOREIGN KEY (Estoque) REFERENCES Estoque(Código)
+);
+
+CREATE TABLE Pagamento (
+	Valor FLOAT8 NOT NULL,
+    Descrição VARCHAR(200),
+    Tipo VARCHAR(50) NOT NULL,
+    Funcionário VARCHAR(11) NOT NULL,
+	CONSTRAINT FOREIGN KEY (Funcionário) REFERENCES Funcionário(CPF)
+);
+
+CREATE TABLE Produto (
+	Código INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Quantidade INTEGER NOT NULL,
+    Descrição VARCHAR(400) NOT NULL,
+    Preço_Unitário FLOAT8 NOT NULL,
+    Preço_Total FLOAT8 NOT NULL,
+    Estoque INTEGER NOT NULL,
+    CONSTRAINT FOREIGN KEY (Estoque) REFERENCES Estoque(Código)
+);
+
+CREATE TABLE Cliente (
+	CPF VARCHAR(11) NOT NULL PRIMARY KEY,
+    Nome VARCHAR(120) NOT NULL,
+    Telefone VARCHAR(21),
+    Gasto_Total FLOAT8 NOT NULL
+);
+
+CREATE TABLE Fornece (
+	Código INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Quantidade INTEGER NOT NULL,
+    Valor FLOAT8 NOT NULL,
+    Fornecedor INTEGER NOT NULL,
+    CONSTRAINT FOREIGN KEY (Fornecedor) REFERENCES Fornecedor(Código),
+    Produto INTEGER NOT NULL,
+    CONSTRAINT FOREIGN KEY (Produto) REFERENCES Produto(Código)
+);
+
+CREATE TABLE Venda (
+	Código INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Tipo VARCHAR(25) NOT NULL,
+    Valor FLOAT8 NOT NULL,
+    Forma_de_Pagamento VARCHAR(60) NOT NULL,
+    Parcelamento VARCHAR(4) NOT NULL,
+    Funcionário VARCHAR(11) NOT NULL,
+    CONSTRAINT FOREIGN KEY (Funcionário) REFERENCES Funcionário(CPF),
+    Cliente VARCHAR(11),
+    CONSTRAINT FOREIGN KEY (Cliente) REFERENCES Cliente(CPF),
+    Franquia INTEGER NOT NULL,
+    CONSTRAINT FOREIGN KEY (Franquia) REFERENCES Franquia(Código)
+);
+
+CREATE TABLE Itens_da_Venda (
+	Item INTEGER NOT NULL,
+    CONSTRAINT FOREIGN KEY (Item) REFERENCES Produto(Código),
+    Venda INTEGER NOT NULL,
+    CONSTRAINT FOREIGN KEY (Venda) REFERENCES Venda(Código)
+);
---
+++ mySQL_BD/Dados.sql
@@ -0,0 +1,72 @@
+INSERT INTO Franquia (Cofre, Cidade)
+VALUES (50290.23, "São Paulo");
+INSERT INTO Franquia (Cofre, Cidade)
+VALUES (3800.34, "Cuiabá");
+INSERT INTO Franquia (Cofre, Cidade)
+VALUES (5921.98, "Goiânia");
+
+INSERT INTO Funcionário (CPF, Nome, Carteira_de_Trabalho, Cargo, Expediente, Salário_Base, Faltas, Venda_do_Mês, Ticket_Médio, Franquia)
+VALUES ("12345678910", "Angela Rezende Kumakura", "222222222222", "Vendedor(a)", "13:45-22:00", 1180.00, 3, 22392.41, 80.85, 2);
+INSERT INTO Funcionário (CPF, Nome, Carteira_de_Trabalho, Cargo, Expediente, Salário_Base, Faltas, Venda_do_Mês, Ticket_Médio, Franquia)
+VALUES ("45612378910", "Maikon", "333333333333", "Vendedor(a)", "09:45-18:00", 1180.00, 0, 17392.41, 115.23, 2);
+INSERT INTO Funcionário (CPF, Nome, Carteira_de_Trabalho, Cargo, Expediente, Salário_Base, Faltas, Venda_do_Mês, Ticket_Médio, Franquia)
+VALUES ("78912345610", "Magna", "111111111111", "Gerente", "09:45-18:00", 1500.00, 0, 200.39, 50.15, 2);
+
+INSERT INTO Funcionário (CPF, Nome, Carteira_de_Trabalho, Cargo, Expediente, Salário_Base, Faltas, Venda_do_Mês, Ticket_Médio, Franquia)
+VALUES ("45678912310", "Andressa", "444444444444", "Vendedor(a)", "13:45-22:00", 1180.00, 2, 19322.41, 90.21, 1);
+INSERT INTO Funcionário (CPF, Nome, Carteira_de_Trabalho, Cargo, Expediente, Salário_Base, Faltas, Venda_do_Mês, Ticket_Médio, Franquia)
+VALUES ("78945612310", "Felipe", "555555555555", "Vendedor(a)", "09:45-18:00", 1180.00, 1, 21402.32, 105.85, 1);
+INSERT INTO Funcionário (CPF, Nome, Carteira_de_Trabalho, Cargo, Expediente, Salário_Base, Faltas, Venda_do_Mês, Ticket_Médio, Franquia)
+VALUES ("12345678911", "Lucas", "666666666666", "Gerente", "09:45-18:00", 1500.00, 3, 590.13, 90.13, 1);
+
+INSERT INTO Funcionário (CPF, Nome, Carteira_de_Trabalho, Cargo, Expediente, Salário_Base, Faltas, Venda_do_Mês, Ticket_Médio, Franquia)
+VALUES ("12345678912", "Jackeline", "777777777777", "Vendedor(a)", "13:45-22:00", 1180.00, 5, 15964.41, 69.96, 3);
+INSERT INTO Funcionário (CPF, Nome, Carteira_de_Trabalho, Cargo, Expediente, Salário_Base, Faltas, Venda_do_Mês, Ticket_Médio, Franquia)
+VALUES ("12345678913", "Pedro", "888888888888", "Vendedor(a)", "09:45-18:00", 1180.00, 1, 18411.91, 130.41, 3);
+INSERT INTO Funcionário (CPF, Nome, Carteira_de_Trabalho, Cargo, Expediente, Salário_Base, Faltas, Venda_do_Mês, Ticket_Médio, Franquia)
+VALUES ("12345678914", "José", "999999999999", "Gerente", "09:45-18:00", 1500.00, 1, 310.13, 82.23, 3);
+
+INSERT INTO Cliente (CPF, Nome, Telefone, Gasto_Total)
+VALUES ("11111111111", "João", "4444444444", 1232.40);
+INSERT INTO Cliente (CPF, Nome, Telefone, Gasto_Total)
+VALUES ("22222222222", "Maria", "55555555555", 122.35);
+INSERT INTO Cliente (CPF, Nome, Telefone, Gasto_Total)
+VALUES ("33333333333", "Caio", "66666666666", 242.99);
+INSERT INTO Cliente (CPF, Nome, Telefone, Gasto_Total)
+VALUES ("66666666666", "Amanda", "4444444444", 5.99);
+INSERT INTO Cliente (CPF, Nome, Gasto_Total)
+VALUES ("77777777777", "Joaquim", 39.99);
+
+INSERT INTO Estoque (Contato, Endereço)
+VALUES ("emailemail@email.com", "Cuiabá");
+INSERT INTO Estoque (Contato, Endereço)
+VALUES ("emailemailado@email.com", "São Paulo");
+INSERT INTO Estoque (Contato, Endereço)
+VALUES ("emailadoemail@email.com", "Goiânia");
+
+INSERT INTO Shopping (Nome, Endereço, Aluguel)
+VALUES ("Shopping Estação Cuiabá", "Cuiabá", 5000.00);
+INSERT INTO Shopping (Nome, Endereço, Aluguel)
+VALUES ("Shopping Novo Mundo", "Goiânia", 6000.00);
+INSERT INTO Shopping (Nome, Endereço, Aluguel)
+VALUES ("Shopping Paulista", "São Paulo", 7000.00);
+
+INSERT INTO Fornecedor (Contato, Endereço)
+VALUES ("123123123", "Cuiabá");
+INSERT INTO Fornecedor (Contato, Endereço)
+VALUES ("321321321", "Goiânia");
+INSERT INTO Fornecedor (Contato, Endereço)
+VALUES ("345345345", "São Paulo");
+
+INSERT INTO Produto (Quantidade, Descrição, Preço_Unitário, Preço_Total, Estoque)
+VALUES (5000, "Papel A4", 0.10, 500.00, 1);
+INSERT INTO Produto (Quantidade, Descrição, Preço_Unitário, Preço_Total, Estoque)
+VALUES (200, "Lapis FaberCastel", 1.25, 250.00, 1);
+INSERT INTO Produto (Quantidade, Descrição, Preço_Unitário, Preço_Total, Estoque)
+VALUES (5000, "Papel A4", 0.10, 500.00, 2);
+INSERT INTO Produto (Quantidade, Descrição, Preço_Unitário, Preço_Total, Estoque)
+VALUES (200, "Lapis FaberCastel", 1.25, 250.00, 2);
+INSERT INTO Produto (Quantidade, Descrição, Preço_Unitário, Preço_Total, Estoque)
+VALUES (5000, "Papel A4", 0.10, 500.00, 3);
+INSERT INTO Produto (Quantidade, Descrição, Preço_Unitário, Preço_Total, Estoque)
+VALUES (200, "Lapis FaberCastel", 1.25, 250.00, 3);
